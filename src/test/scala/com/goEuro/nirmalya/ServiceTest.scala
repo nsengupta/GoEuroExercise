83b68ca2db9d0a4dea7bba1ca503f1bdca6fefe8
nsengupta <giterator@gmail.com>
2015-06-18 23:11:47 +0530

    Fixed tests

--- src/test/scala/com/goEuro/nirmalya/ServiceTest.scala
+++ src/test/scala/com/goEuro/nirmalya/ServiceTest.scala
@@ -32,7 +32,7 @@ class ServiceTest  extends FunSuite with BeforeAndAfter with MockitoSugar {
 
   test("correct parsing for erroneous HttpResponse (server error)") {
 
-    val httpReponse500 = HttpResponse("Some body",500,Map("Status" -> "Server Error"))
+    val httpReponse500 = HttpResponse("Some body", 500, Map("Status" -> "Server Error"))
 
     val listProducer = new CitySuggestionListProducer()
 
@@ -40,91 +40,90 @@ class ServiceTest  extends FunSuite with BeforeAndAfter with MockitoSugar {
 
     assert(parsedInfo.size == 1)
     assert(parsedInfo.head._id == -2)
-    assert(parsedInfo.head.key.get == "Server Error")
-    assert(parsedInfo.head.name.isEmpty == true)
-    assert(parsedInfo.head.geo_position.get == GeoPosition(0.0d,0.0d))
+    assert(parsedInfo.head.key.isEmpty == true)
+    assert(parsedInfo.head.name.get == "Server Error")
+    assert(parsedInfo.head.geo_position.get == GeoPosition(0.0d, 0.0d))
 
   }
 
   test("correct parsing for erroneous HttpResponse (client error)") {
 
-    val httpReponse500 = HttpResponse("Some body",400,Map("Status" -> "Client Error"))
+      val httpReponse500 = HttpResponse("Some body",400,Map("Status" -> "Client Error"))
 
-    val listProducer = new CitySuggestionListProducer()
+      val listProducer = new CitySuggestionListProducer()
 
-    val parsedInfo = listProducer.parseResponse(httpReponse500)
+      val parsedInfo = listProducer.parseResponse(httpReponse500)
 
-    assert(parsedInfo.size == 1)
-    assert(parsedInfo.head._id == -2)
-    assert(parsedInfo.head.key.get == "Client Error")
-    assert(parsedInfo.head.name.isEmpty == true)
-    assert(parsedInfo.head.geo_position.get == GeoPosition(0.0d,0.0d))
+      assert(parsedInfo.size == 1)
+      assert(parsedInfo.head._id == -2)
+      assert(parsedInfo.head.key.isEmpty == true)
+      assert(parsedInfo.head.name.get == "Client Error")
+      assert(parsedInfo.head.geo_position.get == GeoPosition(0.0d,0.0d))
 
-  }
+    }
 
   test("correct extraction of Information from HttpResponse (well-formed JSON)") {
 
-    val jsonBody = "[{\"_id\":376217,\"key\":null,\"name\":\"Berlin\",\"fullName\":\"Berlin, Germany\",\"iata_airport_code\":null,\"itype\":\"location\",\"country\":\"Germany\",\"geo_position\":{\"latitude\":52.52437,\"longitude\":13.41053},\"locationId\":8384,\"inEurope\":true,\"countryCode\":\"DE\",\"coreCountry\":true,\"distance\":null}]"
+      val jsonBody = "[{\"_id\":376217,\"key\":null,\"name\":\"Berlin\",\"fullName\":\"Berlin, Germany\",\"iata_airport_code\":null,\"itype\":\"location\",\"country\":\"Germany\",\"geo_position\":{\"latitude\":52.52437,\"longitude\":13.41053},\"locationId\":8384,\"inEurope\":true,\"countryCode\":\"DE\",\"coreCountry\":true,\"distance\":null}]"
 
-    val sampleHttpResponse = HttpResponse(jsonBody,200,Map("Status" -> "OK"))
+      val sampleHttpResponse = HttpResponse(jsonBody,200,Map("Status" -> "OK"))
 
-    val listProducer = new CitySuggestionListProducer()
+      val listProducer = new CitySuggestionListProducer()
 
-    val infoBunch = listProducer.prepareInformation(sampleHttpResponse)
+      val infoBunch = listProducer.prepareInformation(sampleHttpResponse)
 
-    assert(infoBunch.size == 1)
-    assert(infoBunch.head._id == 376217)
-    assert(infoBunch.head.key.isEmpty == true)
-    assert(infoBunch.head.name.get == "Berlin")
+      assert(infoBunch.size == 1)
+      assert(infoBunch.head._id == 376217)
+      assert(infoBunch.head.key.isEmpty == true)
+      assert(infoBunch.head.name.get == "Berlin")
 
 
   }
 
   test("correct extraction of Information from HttpResponse (malformed JSON)") {
 
-    val jsonBody = "[{\"_id\":376217,key\":null,\"name\":\"Berlin\",\"fullName\":\"Berlin, Germany\",\"iata_airport_code\":null,\"itype\":\"location\",\"country\":\"Germany\",\"geo_position\":{\"latitude\":52.52437,\"longitude\":13.41053},\"locationId\":8384,\"inEurope\":true,\"countryCode\":\"DE\",\"coreCountry\":true,\"distance\":null}]"
+      val jsonBody = "[{\"_id\":376217,key\":null,\"name\":\"Berlin\",\"fullName\":\"Berlin, Germany\",\"iata_airport_code\":null,\"itype\":\"location\",\"country\":\"Germany\",\"geo_position\":{\"latitude\":52.52437,\"longitude\":13.41053},\"locationId\":8384,\"inEurope\":true,\"countryCode\":\"DE\",\"coreCountry\":true,\"distance\":null}]"
 
-    val sampleHttpResponse = HttpResponse(jsonBody,200,Map("Status" -> "OK"))
+      val sampleHttpResponse = HttpResponse(jsonBody,200,Map("Status" -> "OK"))
 
-    val listProducer = new CitySuggestionListProducer()
+      val listProducer = new CitySuggestionListProducer()
 
-    val infoBunch = listProducer.prepareInformation(sampleHttpResponse)
+      val infoBunch = listProducer.prepareInformation(sampleHttpResponse)
 
-    assert(infoBunch.size == 1)
-    assert(infoBunch.head._id == -3)
-    assert(infoBunch.head.key.isEmpty == false)
-    assert(infoBunch.head.name == None)
+      assert(infoBunch.size == 1)
+      assert(infoBunch.head._id == -3)
+      assert(infoBunch.head.key.isEmpty == true)
+      assert(infoBunch.head.name.isEmpty == false)
   }
 
   test("Storing values in CSV retrieved from HttpResponse (well-formed JSON)") {
 
-    val jsonBody = "[{\"_id\":376217,\"key\":null,\"name\":\"Berlin\",\"fullName\":\"Berlin, Germany\",\"iata_airport_code\":null,\"type\":\"location\",\"country\":\"Germany\",\"geo_position\":{\"latitude\":52.52437,\"longitude\":13.41053},\"locationId\":8384,\"inEurope\":true,\"countryCode\":\"DE\",\"coreCountry\":true,\"distance\":null}]"
+      val jsonBody = "[{\"_id\":376217,\"key\":null,\"name\":\"Berlin\",\"fullName\":\"Berlin, Germany\",\"iata_airport_code\":null,\"type\":\"location\",\"country\":\"Germany\",\"geo_position\":{\"latitude\":52.52437,\"longitude\":13.41053},\"locationId\":8384,\"inEurope\":true,\"countryCode\":\"DE\",\"coreCountry\":true,\"distance\":null}]"
 
-    val sampleHttpResponse = HttpResponse(jsonBody,200,Map("Status" -> "OK"))
+      val sampleHttpResponse = HttpResponse(jsonBody,200,Map("Status" -> "OK"))
 
-    val listProducer = new CitySuggestionListProducer("./City-Suggestions.csv")
+      val listProducer = new CitySuggestionListProducer("./City-Suggestions.csv")
 
-    val infoBunch = listProducer.prepareInformation(sampleHttpResponse)
+      val infoBunch = listProducer.prepareInformation(sampleHttpResponse)
 
-    listProducer.saveAsCSV(infoBunch)
+      listProducer.saveAsCSV(infoBunch)
 
-    val reader = CSVReader.open(new File(listProducer.csvFileName))
+      val reader = CSVReader.open(new File(listProducer.csvFileName))
 
-    val rows = reader.allWithHeaders
+      val rows = reader.allWithHeaders
 
-    assert(rows.size == 1)
+      assert(rows.size == 1)
 
-    val firstRowWithColumnHeaders = rows.head
+      val firstRowWithColumnHeaders = rows.head
 
-    assert(firstRowWithColumnHeaders("_id") == "376217")
-    assert(firstRowWithColumnHeaders("name") == "Berlin")
-    assert(firstRowWithColumnHeaders("type") == "location")
-    assert(firstRowWithColumnHeaders("latitude") == "52.52437")
-    assert(firstRowWithColumnHeaders("longitude") == "13.41053")
+      assert(firstRowWithColumnHeaders("_id") == "376217")
+      assert(firstRowWithColumnHeaders("name") == "Berlin")
+      assert(firstRowWithColumnHeaders("type") == "location")
+      assert(firstRowWithColumnHeaders("latitude") == "52.52437")
+      assert(firstRowWithColumnHeaders("longitude") == "13.41053")
 
-    reader.close
+      reader.close
 
   }
-
-
 }
+
